211b5c883213c51637dcac184ff1a454f3c9af5c
DTC <94540275+DTC172@users.noreply.github.com>
2024-07-24 18:45:16 +0700

    first commit

---
+++ lib/components/level.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/components/players.dart';
+
+class Level extends World {
+  final String levelName;
+  final Player player;
+  Level({required this.levelName, required this.player});
+  late TiledComponent level;
+
+  @override
+  FutureOr<void> onLoad() async {
+    level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
+
+    add(level);
+    final spawnPiontsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    for (final spawnPoint in spawnPiontsLayer!.objects) {
+      switch (spawnPoint.class_) {
+        case 'Player':
+          player.position = Vector2(spawnPoint.x, spawnPoint.y);
+          add(player);
+          break;
+        default:
+      }
+    }
+    return super.onLoad();
+  }
+}
